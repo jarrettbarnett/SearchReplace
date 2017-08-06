639935081282a45d5aabb0deccfe1c5c90fe2cd8
Jarrett Barnett <hello@jarrettbarnett.com>
2017-08-06 14:22:15 -0700

    Updated database name to avoid collisions with using dot syntax.

--- tests/SearchReplace/SearchReplaceTest.php
+++ tests/SearchReplace/SearchReplaceTest.php
@@ -12,7 +12,7 @@ class SearchReplaceTest extends TestCase
     const DB_HOST = '127.0.0.1';
     const DB_USER = 'root';
     const DB_PASS = '';
-    const DB_NAME = 'searchreplace.dev';
+    const DB_NAME = 'searchreplace_dev';
 
     public function setUp()
     {
